06147b59d974ba05c399bd732fea30bbbf3dc677
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2024-07-27 01:58:04 +0100

    Update AddBrand.js

--- src/components/pages/dashboard/categories/AddBrand.js
+++ src/components/pages/dashboard/categories/AddBrand.js
@@ -27,7 +27,7 @@ const AddBrand = (_) => {
     setIsLoading(true);
 
     await sendAuthenticatedPostRequest(
-      'https://powerful-pink-antelope.cyclic.app/api/v1/products/newbrand',
+      'https://waves-back-mp2xyzete-altairs-projects-b0a4e876.vercel.app/api/v1/products/newbrand',
       { name: data.name },
       { headers: { Authorization: token } }
     )
