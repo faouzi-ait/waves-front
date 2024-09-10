f696725156e51978a4f2b62f132b10bfed55c8ac
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2024-09-10 20:42:30 +0100

    Update Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -23,7 +23,7 @@ const Contact = (_) => {
         name: data.name,
         lastname: data.lastname,
         email: data.email,
-        subject: "WAVE"
+        subject: "WAVE",
         message: data.note,
       }
     )
